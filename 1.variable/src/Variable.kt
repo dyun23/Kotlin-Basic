bbd47ea17ad6ade801304d0dab6b49e4fbc10074
Dayun Kim <ozsally0201@gmail.com>
2024-11-10 02:12:04 +0900

    1.기본 문법과 변수 선언

---
+++ 1.variable/src/Variable.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hello World!")
+
+    val name1 = "Alice"   // 불변 변수, 값 변경 불가
+    var age1 = 25         // 가변 변수, 값 변경 가능
+
+    val name2: String = "Alice"
+    var age2: Int = 25
+}
